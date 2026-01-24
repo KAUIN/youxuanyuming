b20c17de6e3fd174a74f6bc97b5e65c6e5b32ba8
KAUIN <37978775+KAUIN@users.noreply.github.com>
2026-01-24 13:33:20 +0800

    Update collect_ips.py

--- collect_ips.py
+++ collect_ips.py
@@ -644,8 +644,7 @@ def main():
         'https://api.uouin.com/cloudflare.html',
         'https://www.wetest.vip/page/cloudflare/address_v4.html',
         'https://www.wetest.vip/page/cloudflare/address_v6.html',
-        'https://stock.hostmonit.com/CloudFlareYes',
-        'https://stock.hostmonit.com/CloudFlareYesV6',
+        'https://stock.hostmonit.com',
         'https://cf.090227.xyz/'  # 新增的域名网站
     ]
     
